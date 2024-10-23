44d11f2f35786a820140f207fb0cba54d744069d
IgorPanasik <panasik.igor.com@gmail.com>
2024-10-23 22:30:54 +0300

    default settings for webpack

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,37 @@
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+import webpack from 'webpack';
+import { IBuildOptions } from './types/types';
+
+export const buildPlugins = ({
+	mode,
+	paths,
+}: IBuildOptions): webpack.WebpackPluginInstance[] => {
+	const isDev = mode === 'development';
+	const isProd = mode === 'production';
+
+	const plugins: webpack.WebpackPluginInstance[] = [
+		new HtmlWebpackPlugin({
+			template: paths.html,
+			favicon: path.resolve(path.resolve(paths.public, 'icons/todo.ico')),
+		}),
+	];
+
+	if (isDev) {
+		plugins.push(new ForkTsCheckerWebpackPlugin());
+		plugins.push(new ReactRefreshWebpackPlugin());
+	}
+
+	if (isProd) {
+		plugins.push(
+			new MiniCssExtractPlugin({
+				filename: 'css/[name].[contenthash:8].css',
+				chunkFilename: 'css/[name].[contenthash:8].css',
+			})
+		);
+	}
+	return plugins;
+};
